28887aae6533cd3027a1198aace7e9f9f9b91aab
nicolasattilio <nicolasattilio@gmail.com>
2019-03-30 11:55:15 -0300

    finalizando ejercicio 1

--- practico-1/src/ejercicio1/Persona.java
+++ practico-1/src/ejercicio1/Persona.java
@@ -1,12 +1,11 @@
 package ejercicio1;
 
-import javax.print.attribute.standard.PrinterLocation;
 
 public class Persona {
 	private String nombre="N";
 	private String apellido="N";
 	private int edad;
-	private String fecha_nacimiento= "1 Enero de 2000";
+	private String fechaNacimiento= "01-01-2016";
 	private int dni;
 	private char sexo='F';
 	private int peso=1;
@@ -21,18 +20,90 @@ public class Persona {
 		this.apellido=apellido;						
 		
 	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
 	public Persona(int d, String nombre, String apellido, String fecha) {
 		this.dni=d;
 		this.nombre=nombre;
 		this.apellido=apellido;
-		this.fecha_nacimiento=fecha;
+		this.fechaNacimiento=fecha;
 	}
 	
+	public double masaCorporal() {
+		return this.peso/(this.altura*this.altura);
+	}
+	
+	public boolean estaEnForma() {
+		if ((18.5 < this.masaCorporal()) && (25>this.masaCorporal()))
+			return true;
+		return false;
+	}
+	
+	public boolean cumpleaños() {
+	//preguntar en clase
+		return true;
+	}
+	
+	public boolean puedeVotar() {
+		if (this.edad>16)
+			return true;
+		return false;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	
+	public boolean mayorEdad() {
+		if (this.edad>18)
+			return true;
+		return false;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Persona n1 = new Persona(36882126);
-		System.out.println(n1.nombre);
+		Persona n1 = new Persona(36882126, "Nicolas","Apellido");
+		System.out.println(n1.getNombre());
 
 	}
 
