6ea9b446b95cc973698799a77d00b83304d80ef1
nicolasattilio <nicolasattilio@gmail.com>
2019-04-01 17:08:07 -0300

    Implementando clase Rectangulo P2E3

--- practico-2/src/ejercicio3/Punto.java
+++ practico-2/src/ejercicio3/Punto.java
@@ -19,30 +19,35 @@ public class Punto {
 		return coorY;
 	}
 	
+	public void setCoorX(double coorX) {
+		this.coorX = coorX;
+	}
+
+	public void setCoorY(double coorY) {
+		this.coorY = coorY;
+	}
+
 	public void devolverPunto() {
 		System.out.println(this.getCoorX()+","+this.getCoorY());
 	}
 	
 	public double distanciaEuclidia(Punto p2) {
-		double dx=((this.getCoorX()-p2.getCoorX())*(this.getCoorX()-p2.getCoorX()));
-		double dy=((this.getCoorY()-p2.getCoorY())*(this.getCoorY()-p2.getCoorY()));
-		double dtotal=dx+dy;
-		return (dtotal*dtotal);
-		
+		return Math.sqrt((Math.pow((this.getCoorX()-p2.getCoorX()),2))+(Math.pow((this.getCoorY()-p2.getCoorY()),2)));	
 	}
 	
 	public void desplazarPunto(double x,double y) {
-		this.coorX=x;
-		this.coorY=y;
+		this.setCoorX(x);
+		this.setCoorY(y);
 	}
 
 	public static void main(String[] args) {
-		Punto p1=new Punto(2,3);
-		p1.desplazarPunto(3,4);
-		p1.devolverPunto();
-		Punto p2=new Punto(5,3);
+		Punto p1=new Punto(2,1);
+		Punto p2=new Punto(5,1);
+		Punto p3=new Punto(5,3);
+		Punto p4=new Punto(2,3);
 		System.out.println(p1.distanciaEuclidia(p2));
-		
+		Rectangulo r1=new Rectangulo(p1, p2, p3, p4);
+		System.out.println(r1.esCuadrado());
 	}
 
 }
